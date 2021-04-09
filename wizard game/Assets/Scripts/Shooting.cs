84d931e8af0e680c5cf775ce077e3214db7b3542
anwersaad0 <anwer.saad0@gmail.com>
2021-04-09 00:12:38 -0700

    Player can now shoot projectiles

---
+++ wizard game/Assets/Scripts/Shooting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePt;
+    public GameObject bulletPrefab;
+
+    public float force = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePt.position, firePt.rotation);
+        Rigidbody2D rBody = bullet.GetComponent<Rigidbody2D>();
+        rBody.AddForce(firePt.up * force, ForceMode2D.Impulse);
+    }
+}
